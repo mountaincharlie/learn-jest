7fb1c9a71e3137a2747e1846760ef8146eb90468
Charlie <ciharland@hotmail.co.uk>
2022-02-18 12:26:38 +0000

    Add newGame function and tests

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -6,5 +6,19 @@ let game = {
     choices: ["button1", "button2", "button3", "button4"],
 };
 
+// resets game.score and the currentGame/playerMoves arrays
+function newGame(){
+    game.score = 0;
+    game.currentGame = [];
+    game.playerMoves = [];
+    showScore()
+};
+
+// shows the score to the user
+function showScore(){
+    document.getElementById("score").innerText = game.score;
+};
+
+
 // exporting game functions for testing
-module.exports = {game};  // using {} since we're exporting more than 1 object
+module.exports = {game, newGame, showScore};  // using {} since we're exporting more than 1 object
--- assets/scripts/tests/game.test.js
+++ assets/scripts/tests/game.test.js
@@ -3,7 +3,7 @@
  */
 
 // getting the file we're testing 
-const {game} = require("../game");
+const {game, newGame, showScore} = require("../game");
 
 // bringing in the mock DOM
 beforeAll(() => {
@@ -30,5 +30,29 @@ describe("game object contains correct keys", () => {
     });
     test("choices contains correct ids", () => {
         expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
+    });
+});
+
+// tests for the newGame function
+describe("newGame works correctly", () => {
+    // setting up some pretend code to see if the reset works
+    beforeAll(() => {
+        game.score = 42;
+        game.currentGame = ["button1", "button3"];
+        game.playerMoves = ["button4", "button2"];
+        document.getElementById("score").innerText = "42";
+        newGame();
+    });
+    test("should reset game score to 0", () => {
+        expect(game.score).toEqual(0);
+    });
+    test("should clear currentGame array", () => {
+        expect(game.currentGame.length).toEqual(0);
+    });
+    test("should clear playerMoves array", () => {
+        expect(game.playerMoves.length).toEqual(0);
+    });
+    test("should display 0 for element with id = score", () => {
+        expect(document.getElementById("score").innerText).toEqual(0);
     });
 });
